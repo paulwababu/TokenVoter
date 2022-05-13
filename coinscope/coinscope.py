2903d216dd476fd64af78e85c5af76d0c6ab6969
lilsidoh <asidoihsidney254@gmail.com>
2022-05-13 12:33:16 +0300

    twitter registration working

--- coinscope/coinscope.py
+++ coinscope/coinscope.py
@@ -10,70 +10,102 @@ import time, tqdm, json
 import random
 
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
 
 import mails
 from configs import Gleam
 class Coinscope:
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self):
+        pass
 
-    def RegisterTwitter(self, queue):
+    def RegisterTwitter(driver, queue):
         while not queue.empty():
+            user = 0
             credentials = queue.get_nowait()
             if len(credentials) == 0:
                 pass
             try:
+                gleam = Gleam(12323, "51.161.196.188",  "abdul9999", "Asdfqwer1")
+                driver = gleam.ChromeSetUp(use_proxy=True, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
                 userName = credentials.split(":")[0].split("@")[0]
                 email    = credentials.split(":")[0]
                 password = credentials.split(":")[1]
-                self.driver.get("https://twitter.com/signup")
-                WebDriverWait(self.driver, 120).until(Ec.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]"))).click()
-                self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[3]").click()
-                self.driver.find_element(By.NAME, "name").send_keys(userName)
-                self.driver.find_element(By.NAME, "email").send_keys(email)
-                Select(self.driver.find_element(By.ID, "SELECTOR_1")).select_by_index(random.randint(1, 12))
-                Select(self.driver.find_element(By.ID, "SELECTOR_2")).select_by_index(random.randint(1, 31))
-                Select(self.driver.find_element(By.ID, "SELECTOR_3")).select_by_index(random.randint(1, 50))
+                unregistered = open("unregistered.json", "w+")
+                driver.get("https://twitter.com/signup")
+                WebDriverWait(driver, 120).until(Ec.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]"))).click()
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[3]").click()
+                driver.find_element(By.NAME, "name").send_keys(userName)
+                driver.find_element(By.NAME, "email").send_keys(email)
+                Select(driver.find_element(By.ID, "SELECTOR_1")).select_by_index(random.randint(1, 12))
+                Select(driver.find_element(By.ID, "SELECTOR_2")).select_by_index(random.randint(1, 31))
+                Select(driver.find_element(By.ID, "SELECTOR_3")).select_by_index(random.randint(25, 50))
                 time.sleep(2)
-                self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div").click()
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div").click()
                 time.sleep(1)
-                self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div").click()
-                time.sleep(10)
-                self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div").click()
-                code = mails.webmail_login_cpanel(email, password).split(" ")[0]
-                print(code, email, password)
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div").click()
+                time.sleep(5)
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[2]/div").click()
+                code = mails.webmail_login_cpanel(email, password)
+                code = ""
+                if code.isdigit() == False:
+                    time.sleep(5)
+                    code = mails.webmail_login_cpanel(email, password)
+                    if code == None:
+                        print("[*] length code error reloading")
+                    code = code
+                    
+                if code == None:
+                    print("length is none")
+                    pass   
                 time.sleep(1)
-                self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input").send_keys(1234)
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input").send_keys(code)
                 time.sleep(1)
-                self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/main/div/div/div/div[2]/div[2]/div[2]/div/div/div/div").click()
-                time.sleep(20)
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div").click()
+                WebDriverWait(driver, 5).until(Ec.presence_of_element_located((By.NAME, "password"))).send_keys(password)
+                time.sleep(5)
+                driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div").click()
+                time.sleep(15)
+                user += 1
+                print(f"[*] Created {email}")
+                driver.close()
             
             except StaleElementReferenceException as error:
                 print(error)
                 pass
+        
+            except NoSuchElementException as error:
+                print(error)
+                data = {
+                    "email" : email,
+                    "password" : password
+                }
+                pass 
 
     def Voter(self, credentials):
+        gleam = Gleam(12323, "51.161.196.188",  "abdul9999", "Asdfqwer1")
+        driver = gleam.ChromeSetUp(use_proxy=True, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
         userName = credentials.split(":")[0].split("@")[0]
         email    = credentials.split(":")[0]
         password = credentials.split(":")[1]
         self.driver.get("https://www.coinscope.co/login")
         time.sleep(2)
-        WebDriverWait(self.driver, 50).until(Ec.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/main/div/div/div[2]/div/div/div[1]/form/ul/li[2]/button"))).click()
+        WebDriverWait(self.driver, 30).until(Ec.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/main/div/div/div[2]/div/div/div[1]/form/ul/li[2]/button"))).click()
         currentWindow = self.driver.window_handles[0]
-        time.sleep(10)
+        time.sleep(5)
         windowAfter = self.driver.window_handles[1]
         self.driver.switch_to.window(windowAfter)
-        time.sleep(20)
+        time.sleep(5)
         self.driver.find_element(By.ID, "username_or_email").send_keys(email)
         self.driver.find_element(By.ID, "password").send_keys(password)
         self.driver.find_element(By.ID, "allow").submit()
-        WebDriverWait(self.driver, 10).until(Ec.presence_of_element_located((By.ID, "challenge_response"))).send_keys("0712075893")
-        WebDriverWait(self.driver, 10).until(Ec.presence_of_element_located((By.ID, "email_challenge_submit"))).submit()
-        time.sleep(20)
+        WebDriverWait(self.driver, 5).until(Ec.presence_of_element_located((By.ID, "challenge_response"))).send_keys("0712075893")
+        WebDriverWait(self.driver, 5).until(Ec.presence_of_element_located((By.ID, "email_challenge_submit"))).submit()
+        time.sleep(5)
         self.driver.switch_to.window(currentWindow)
-        time.sleep(20)
+        time.sleep(5)
         self.driver.get("https://www.coinscope.co/coin/gwolf")
         WebDriverWait(self.driver, 5).until(Ec.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[1]/div/div[3]/div[1]/div[2]/button[1]"))).submit()
+        time.sleep(10)
         self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/main/div/div[2]/div[3]/button").submit()
         time.sleep(10)
         self.driver.close()
@@ -83,16 +115,16 @@ def AddEmails(queue):
     data = open("credetials.json", "r")
     emailJson = json.loads(data.read())
     for credentials in emailJson:
-        email = credentials["email"] + '@vedsredec.com'
+        email = credentials["email"] + '@vedroy.xyz'
         password = credentials["password"]
         userPassword = f"{email}:{password}"
         if queue.full() != True:
             queue.put(userPassword)
 
-def CreateThreads(driver):
+def CreateThreads():
     queue = Queue()
     AddEmails(queue=queue)
-    coinscope = Coinscope(driver)
+    coinscope = Coinscope()
     for thread in range(0, 3):
         worker = Thread(target=coinscope.RegisterTwitter, args=(queue, ))
         worker.start()
@@ -101,8 +133,6 @@ def CreateThreads(driver):
 
 
 if __name__ == "__main__":
-    gleam = Gleam(12323, "51.161.196.188",  "abdul9999", "Asdfqwer1")
-    driver = gleam.ChromeSetUp(use_proxy=True, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
     #coinscope = Coinscope(driver)
     #coinscope.Voter("asidohsidney254@gmail.com:Asidoh254")
-    CreateThreads(driver=driver)
+    CreateThreads()
--- coinscope/mails.py
+++ coinscope/mails.py
@@ -1,5 +1,4 @@
-from curses.ascii import EM
-from distutils.log import error
+from multiprocessing.connection import wait
 from re import sub
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,56 +13,69 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.firefox.options import Options
+from tables import restrict_flavors
+
+
+from selenium.common.exceptions import UnexpectedAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import TimeoutException
 
 def webmail_login_cpanel(email, password):
     #Open firefox
     s=Service("/home/dev/personalProjects/upwork/wolf/geckodriver")
     #set headless
     options = Options()
-    options.headless = False
-    browser = webdriver.Firefox(service=s, options=options)#
+    options.headless = True
+    browser = webdriver.Firefox(service=s, options=options)
     browser.get("https://vedroy.xyz:2096/")
+    unregistered  = open("unregistered.json", "w+")
     try:
-        print("Inputing login credentials")
-        email = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='user']"))).send_keys(email)
-        password = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='pass']"))).send_keys(password)
-        time.sleep(10)
-    except:
-        print("Failed... Repeating loop")
-        exit()
-    time.sleep(1)
-    try:
-        print("Clicking login button")
-        login = browser.find_element_by_id("login_submit").click() 
-    except:
-        print("Failed... Repeating loop")
-        exit()       
-    time.sleep(3)
-    try:
-        print("Click open on roundcube landing page")
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='launchActiveButton']"))).click()    
-    except:
-        print("Failed... Repeating loop")
-        exit()
-    try:
-        print("Click refresh")
-        time.sleep(30)
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='rcmbtn111']"))).click()      
-    except:
-        print("Failed... ending loop")
-        exit()        
-    try:
-        print("Getting latest email")
-        latest_email = browser.find_elements(By.CLASS_NAME, "message.unread")[0].get_attribute("innerHTML")
-        print(latest_email)
-    except:
-        print("Failed to get latest... Repeating loop, waiting 30 seconds before retrying and exiting")
-        time.sleep(30)
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='rcmbtn111']"))).click()
-        latest_email = browser.find_element_by_id("rcmrowMQ").text
-        print(latest_email)
-        return latest_email
-    return latest_email    
+        email = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='user']"))).send_keys(email)
+        password = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='pass']"))).send_keys(password)
+        login = browser.find_element(By.ID, "login_submit").click() 
+        WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.ID, "activeClientLogoContainer"))).click()     
+        latest_email = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/table[2]/tbody/tr[1]/td[2]/span[3]/a/span").get_attribute("innerHTML")
+        print(latest_email.split(" ")[0])
+        if len(latest_email) == 0:
+            time.sleep(4)
+            browser.refresh()
+            latest_email = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/table[2]/tbody/tr[1]/td[2]/span[3]/a/span").get_attribute("innerHTML")
+            print(latest_email.split(" ")[0])
 
+        return latest_email.split(" ")[0]
+
+    except UnexpectedAlertPresentException:
+        try:
+            print("unexepcted error occured")
+            browser.refresh()   
+            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.ID, "activeClientLogoContainer"))).click()  
+            latest_email = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/table[2]/tbody/tr[1]/td[2]/span[3]/a/span").get_attribute("innerHTML")
+            print(latest_email)
+            return latest_email
+            
+        except UnexpectedAlertPresentException:
+            data = {
+                    "email" : email,
+                    "password" : password
+                }
+            pass 
+    
+    except NoSuchElementException:
+        return email
+
+    except WebDriverException:
+        try:
+            time.sleep(2)
+            browser.refresh()
+            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.ID, "activeClientLogoContainer"))).click()  
+            latest_email = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/table[2]/tbody/tr[1]/td[2]/span[3]/a/span").get_attribute("innerHTML")
+            print(latest_email)
+            return latest_email
+
+        except TimeoutException:
+            print(
+                ("time out fetching email")
+            )
 if __name__ == '__main__':
-    webmail_login_cpanel("cidoFirst@vedroy.xyz", "to&nXZ;*Lzne")    
+    webmail_login_cpanel("marywoods7d6@vedroy.xyz", "=i6v_^F621#aB!")    
